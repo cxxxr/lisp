9a62c1ad4f4ce23b658ba6847d4cc048abdf423e
cxxxr <g23tlm@gmail.com>
2021-09-04 02:11:58 +0900

    add eval for function

--- src/lisp/eval.rs
+++ src/lisp/eval.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::rc::Rc;
 
-use super::object::{Object, ObjectKind, Error, Cons};
+use super::object::{self, Cons, Error, Object, ObjectKind};
 
 struct Env {
     parent: Option<Box<Env>>,
@@ -33,12 +33,42 @@ fn eval_internal(x: Object, env: &mut Env) -> Result<Object, Error> {
         ObjectKind::Nil | ObjectKind::Fixnum(_) | ObjectKind::Func(_) => Ok(x),
         ObjectKind::Symbol(s) => env.get(s).ok_or(Error::UnboundVariable(s.to_string())),
         ObjectKind::Cons(list) => {
-            unimplemented!()
+            let mut iter = list.iter();
+            let first = iter.next().unwrap();
+            let first = eval_internal(first, env)?;
+            let func = match &*first {
+                ObjectKind::Func(func) => func,
+                _ => return Err(Error::MismatchType(first)),
+            };
+            let mut args = Vec::new();
+            for arg in iter {
+                args.push(eval_internal(arg, env)?);
+            }
+            func(&args)
         }
     }
 }
 
+fn plus(args: &[Object]) -> Result<Object, Error> {
+    let mut acc = 0;
+    for arg in args {
+        match **arg {
+            ObjectKind::Fixnum(n) => {
+                acc += n;
+            }
+            _ => (),
+        }
+    }
+    Ok(object::fixnum(acc))
+}
+
+fn global_env() -> Env {
+    let mut genv = Env::new();
+    genv.set("+", Object::new(ObjectKind::Func(plus)));
+    genv
+}
+
 pub fn eval(x: Object) -> Result<Object, Error> {
-    let mut e = Env::new();
+    let mut e = global_env();
     eval_internal(x, &mut e)
 }
